01740cb344d4a71159f34d08f9667b5ed8dec5b9
qiye-in-work <qiye5757@126.com>
2018-08-01 21:43:51 +0800

    PHP同源策略之cookie跨域问题

---
+++ testCookie/testCookie1/index.php
@@ -0,0 +1,10 @@
+<?php
+
+setcookie("test", 111, 0, '/', 'testcookie1.com');
+
+?>
+
+<script>
+
+document.cookie="test2=222";
+</script>
---
+++ testCookie/testCookie2/index.php
@@ -0,0 +1,9 @@
+<?php
+
+
+?>
+
+<script>
+
+document.cookie="test2=222";
+</script>
