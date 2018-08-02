84537807960266a9e5083818c19c95fef642ea6d
qiye-in-work <qiye5757@126.com>
2018-08-02 21:05:13 +0800

    CORS跨域问题学习（一）

--- testCookie/testCookie1/index.php
+++ testCookie/testCookie1/index.php
@@ -1,10 +1,11 @@
-<?php
 
-setcookie("test", 111, 0, '/', 'testcookie1.com');
-
-?>
-
+<script src="https://apps.bdimg.com/libs/jquery/2.1.4/jquery.min.js">
+</script>
 <script>
-
-document.cookie="test2=222";
+  $.ajax({
+      url: "http://www.testcookie2.com/index.php",
+      success: function(){
+      	console.log(11);
+      }
+  })
 </script>
--- testCookie/testCookie2/index.php
+++ testCookie/testCookie2/index.php
@@ -1,9 +1,3 @@
 <?php
-
-
+   header('Access-Control-Allow-Origin:http://www.testcookie1.com');
 ?>
-
-<script>
-
-document.cookie="test2=222";
-</script>
